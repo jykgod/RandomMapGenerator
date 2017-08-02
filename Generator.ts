4cd57dc1696236358e1c26b8ee501f1a29a274f8
jyk <513303276@qq.com>
2017-08-03 00:38:55 +0800

    添加房间随机生成函数

--- Generator.ts
+++ Generator.ts
@@ -1,3 +1,19 @@
 class Generator{
-    
+    roomList:Room[];
+    tilePixel:number = 16;
+    minLengthOfRoom:number = 8;
+    maxLengthOfRoom:number = 28;
+    stepOneGenerateRooms(r:number){
+        
+    }
+
+    generateAPointInCircle(id:number,r:number):Room{
+        var t:number = Math.random();
+        t = 2 * Math.PI * t;
+        r = Math.random() * r;
+        var w = Math.round(this.minLengthOfRoom + Math.random() * (this.maxLengthOfRoom - this.minLengthOfRoom)) * this.tilePixel;
+        var h = Math.round(this.minLengthOfRoom + Math.random() * (this.maxLengthOfRoom - this.minLengthOfRoom)) * this.tilePixel;
+        var newRoom = new Room(id,Math.cos(t) * r,Math.sin(t) * r,w,h);
+        return newRoom;
+    }
 }
